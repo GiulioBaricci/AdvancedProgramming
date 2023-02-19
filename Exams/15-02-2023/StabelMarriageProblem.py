c27ae0c5654141c11717ddfc5a50b00cf6602c8b
GiulioBaricci <88330510+GiulioBaricci@users.noreply.github.com>
2023-02-19 13:07:27 +0100

    StableMarriageProblem

---
+++ Exams/15-02-2023/StabelMarriageProblem.py
@@ -0,0 +1,35 @@
+def find_stable(menDict, womenDict):
+
+    currentMenDict = {}
+    currentWomenDict = {}
+    currentMenDict.update(menDict)
+    currentWomenDict.update(womenDict)
+    return find_couples(menDict, womenDict, currentMenDict, currentWomenDict, [])
+
+
+def find_couples(menDict, womenDict, currentMenDict, currentWomenDict, couples):
+
+    menList = [manKey for manKey in currentMenDict]
+    womenList = [womenKey for womenKey in currentWomenDict]
+
+    if(len(menList) == 0):
+        return couples
+    else:
+        coppia = [(man, woman) for man, woman in couples if menDict[menList[0]][0] == woman]
+        if(len(coppia) > 0):
+            
+            if(womenDict[coppia[0][1]].index(coppia[0][0]) < womenDict[coppia[0][1]].index(menList[0])):
+                menDict[menList[0]] = menDict[menList[0]][1:]
+            else:
+                currentMenDict[coppia[0][0]] = menDict[coppia[0][0]]
+                couples.remove(coppia[0])
+            
+            return find_couples(menDict, womenDict, currentMenDict, currentWomenDict, couples)
+
+        else:
+            couples.append((menList[0],menDict[menList[0]][0]))
+
+            menDict[menList[0]] = menDict[menList[0]][1:]
+            currentMenDict.pop(menList[0])
+
+            return find_couples(menDict, womenDict, currentMenDict, currentWomenDict, couples)
